7a6a7c6d18487c101ab12b406f210eb66b896a61
zhs007 <sssxueren@gmail.com>
2015-12-09 18:34:38 +0800

    ver 0.2.0

--- src/proj.js
+++ src/proj.js
@@ -1,31 +1,7 @@
 "use strict";
 
 var path = require('path');
-var fs = require('fs');
-var handlebars = require('handlebars');
-var glob = require('glob');
-
-function makedir(dir) {
-    let arr = dir.split(path.sep);
-    let cur = '.';
-    for (let ii = 0; ii < arr.length; ++ii) {
-        cur = path.join(cur, arr[ii]);
-        if (!fs.existsSync(cur)) {
-            fs.mkdirSync(cur);
-        }
-    }
-}
-
-function procFile(name, src, params) {
-    let fobj = path.parse(name);
-    makedir(fobj.dir);
-    let buf = fs.readFileSync(src, 'utf-8');
-
-    let tmp = handlebars.compile(buf);
-    let bufout = tmp(params);
-
-    fs.writeFileSync(name, bufout);
-}
+var zhandlebars = require('zhandlebars');
 
 function procParams(params) {
     params.projname_lc = params.projname.toLowerCase();
@@ -34,49 +10,10 @@ function procParams(params) {
     return params;
 }
 
-function procJson(params, filename) {
-    let bufjson = fs.readFileSync(path.join(__dirname, '../template', filename), 'utf-8');
-    let tmpjson = handlebars.compile(bufjson);
-    let bufout = tmpjson(params);
-    let objout = JSON.parse(bufout);
-
-    let ret = [];
-    for (let ii = 0; ii < objout.length; ++ii) {
-        let cur = objout[ii];
-        if (cur.type == 'dir') {
-            ret.push(cur);
-        }
-        else {
-            let srcfilename = path.join(__dirname, '../template', cur.src);
-            if (fs.existsSync(srcfilename)) {
-                cur.src = srcfilename;
-                ret.push(cur);
-            }
-            else {
-                let arr = glob.sync(srcfilename);
-                for (let jj = 0; jj < arr.length; ++jj) {
-                    let cc = {name: path.join(path.dirname(cur.name), path.basename(arr[jj])), src: arr[jj], type: cur.type};
-                    ret.push(cc);
-                }
-            }
-        }
-    }
-
-    return ret;
-}
-
 function procProj(params) {
     params = procParams(params);
 
-    let objJson = procJson(params, 'main.json');
-    for (let ii = 0; ii < objJson.length; ++ii) {
-        if (objJson[ii].type == 'dir') {
-            makedir(objJson[ii].name);
-        }
-        else {
-            procFile(objJson[ii].name, objJson[ii].src, params);
-        }
-    }
+    zhandlebars.procProj(params, path.join(__dirname, '../template', 'main.json'), path.join(__dirname, '../template'));
 }
 
 exports.procProj = procProj;
